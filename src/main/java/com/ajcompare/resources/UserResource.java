762aa8cbe89619bab722f6b2b7661508f2047fb9
Jorn Kersten <i483331@fhict.nl>
2023-01-11 09:20:08 +0100

    Removed all system.out.println logs

--- src/main/java/com/ajcompare/resources/UserResource.java
+++ src/main/java/com/ajcompare/resources/UserResource.java
@@ -34,7 +34,6 @@ public class UserResource {
     @Path("/register")
     public Response createUser(User user) {
         User newUser = userService.createUser(user);
-        System.out.println("temp test");
         return Response.created(URI.create("/api/user/" + newUser.getName())).build();
     }
 }
